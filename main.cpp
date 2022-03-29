1b4b5b8e17feb8008546ab56f223a45b9dd8f4fe
Maciej Sikora <maciejsikora2302@gmail.com>
2022-03-29 21:34:15 +0200

    current state

--- main.cpp
+++ main.cpp
@@ -1,34 +1,122 @@
 #include <iostream>
 #include <random>
 #include <omp.h>
+#include <string>
+#include <fstream>
 
-const int SIZE = 1000000;
+const int THREADS = 4;
+const int SIZE = 1000000; //musi być większy niż THREADS*100
 double array[SIZE];
 
-void do_work(int threads) {
-  std::uniform_real_distribution<double> distribution(0, 1);
-  double t0 = omp_get_wtime();
-  #pragma omp parallel num_threads(threads)
-  {
-    #pragma omp single
+const int TIMES_TO_REPEAT = 25;
+
+const int STATIC = 1; //domyślnie liczba_iteracji / liczba_wątków, każdy wątek dostaje chunk do wykonania
+const int DYNAMIC = 2; //domyślnie 1, każdy wątek bierze kolejny chunk
+const int GUIDED = 3; //chunki skalują się z ilością nie przydzielonych jeszcze porcji. liczba_nie_przydzielonych_iteracji / liczba_wątków i zmierzają do 1
+const int RUNTIME = 4; //bierze z zmiennej środowiskowej jedną z pozostałych opcji
+
+const int DEFAULT = 1;
+const int MODIFIED = 2;
+
+void do_work(int threads, int schedule_style, int mode) {
+    std::uniform_real_distribution<double> distribution(0, 1);
+    double t0 = omp_get_wtime();
+    #pragma omp parallel num_threads(threads)
     {
-      std::cout << "env:Threads: " << omp_get_num_threads() << std::endl;
+
+
+        std::default_random_engine generator; 
+
+        switch (schedule_style){
+            case STATIC:
+                if(mode == DEFAULT){
+                    #pragma omp for schedule(static)
+                    for (int i = 0; i < SIZE; i++) {
+                        array[i] = distribution(generator);
+                    }
+                }else if(mode == MODIFIED){
+                    #pragma omp for schedule(static, 1)
+                    for (int i = 0; i < SIZE; i++) {
+                        array[i] = distribution(generator);
+                    }
+                }
+                break;
+            case DYNAMIC:
+                if(mode == DEFAULT){
+                    #pragma omp for schedule(dynamic)
+                    for (int i = 0; i < SIZE; i++) {
+                        array[i] = distribution(generator);
+                    }
+                }else if(mode == MODIFIED){
+                    #pragma omp for schedule(dynamic, SIZE/(omp_get_num_threads()*100))
+                    for (int i = 0; i < SIZE; i++) {
+                        array[i] = distribution(generator);
+                    }
+                }
+                break;
+            case GUIDED:
+                #pragma omp for schedule(guided)
+                for (int i = 0; i < SIZE; i++) {
+                    array[i] = distribution(generator);
+                }
+                break;
+        }
+    }
+    double time = omp_get_wtime() - t0;
+    std::ofstream f;
+    if(schedule_style == STATIC && mode == DEFAULT){
+        f.open("f1.txt");
+    }else if(schedule_style == STATIC && mode == MODIFIED){
+        f.open("f2.txt");
+    }else if(schedule_style == DYNAMIC && mode == DEFAULT){
+        f.open("f3.txt");
+    }else if(schedule_style == DYNAMIC && mode == MODIFIED){
+        f.open("f4.txt");
+    }else if(schedule_style == GUIDED && mode == DEFAULT){
+        f.open("f5.txt");
     }
 
-    std::default_random_engine generator; 
-    
-    #pragma omp for schedule(static)
-    for (int i = 0; i < SIZE; i++) {
-      array[i] = distribution(generator);
-    }
-  }
-  double time = omp_get_wtime() - t0;
-  std::cout << time << std::endl;
+    f << "," << time;
 }
 
+std::ofstream get_file(int style, int mode, std::ofstream f1, std::ofstream f2, std::ofstream f3, std::ofstream f4, std::ofstream f5){
+    
+}
 
-int main() {
-  for (int i = 1; i <= 4; i++) {
-    do_work(i);
-  }
+int main(int argc, char* argv[]) {
+
+    std::ofstream f1("f1.txt");
+    std::ofstream f2("f2.txt");
+    std::ofstream f3("f3.txt");
+    std::ofstream f4("f4.txt");
+    std::ofstream f5("f5.txt");
+
+    f1 << "STATIC,default," << SIZE/THREADS;
+    f2 << "STATIC,modified," << 1;
+    f3 << "DYNAMIC,default," << 1;
+    f4 << "DYNAMIC,modified," << SIZE/(THREADS*100);
+    f5 << "GUIDED,default," << "to_allocate/threads";
+
+    int schedule_style[] = {STATIC,DYNAMIC,GUIDED}; 
+    int modes[] = {DEFAULT, MODIFIED};
+    
+    for (int i = 0; i < TIMES_TO_REPEAT; i++){
+        for (int threads = 1; threads <= 4; threads++) {
+            for (int ss = 0; ss < 3; ss++){
+                int style = schedule_style[ss];
+                for (int m = 0; m < 2; m++){
+                    int mode = modes[m];
+                    if (style == MODIFIED && mode == GUIDED) break;
+                    do_work(threads, style, mode);
+                }
+            }
+        }
+    }
+
+    f1 << "\n";
+    f2 << "\n";
+    f3 << "\n";
+    f4 << "\n";
+    f5 << "\n";
+    return 0;
 }
--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
+echo "" > f1.txt
+echo "" > f2.txt
+echo "" > f3.txt
+echo "" > f4.txt
+echo "" > f5.txt
+
 g++ -Wall ./main.cpp -fopenmp -std=c++11
 ./a.out
+
+cat *.txt > gathered.txt
